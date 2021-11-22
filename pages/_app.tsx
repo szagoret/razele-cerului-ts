55e708989d85ee26e6033b86b8c133d1cca47bc0
Sergiu Zagoret <szagoret@complexica.com>
2021-11-22 22:35:07 +0200

    All

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import Head from 'next/head';
+import {AppProps} from 'next/app';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {CacheProvider, EmotionCache} from '@emotion/react';
+import theme from '../src/styles/theme';
+import createEmotionCache from '../src/styles/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const MyApp = (props: MyAppProps) => {
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>My page</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+};
+
+export default MyApp;
---
+++ src/components/Cantare.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography'
+
+const Cantare = ({song}) => {
+
+    return (
+        <Card sx={{minWidth: 275}}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {song.index} {song.title}
+                </Typography>
+                <Typography variant="body2" sx={{whiteSpace: 'pre-wrap'}}>
+                    {song.lyrics}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Cantare;
