6bddd62103469180b4bd91fec6ec0d4b203503a8
Sergiu Zagoret <szagoret@complexica.com>
2021-11-23 23:40:44 +0200

    mda

--- src/components/SlideShowSong.tsx
+++ src/components/SlideShowSong.tsx
@@ -10,7 +10,6 @@ import Typography from '@mui/material/Typography';
 import CloseIcon from '@mui/icons-material/Close';
 import Slide from '@mui/material/Slide';
 import {TransitionProps} from '@mui/material/transitions';
-import {MobileStepper} from "@mui/material";
 import {KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material";
 import SwipeableViews from 'react-swipeable-views';
 import {Box} from "@mui/system";
@@ -51,27 +50,14 @@ const SlideShowSong = ({song, open, handleClose}: SlideShowSongPropsType) => {
             onClose={handleClose}
             TransitionComponent={Transition}
         >
-            <AppBar position="sticky" color="primary" sx={{top: 0, bottom: 'auto'}}>
-                <Toolbar>
-                    <Typography sx={{ml: 2, flex: 1}} variant="h6" component="div">
-                        {song.title}
-                    </Typography>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        onClick={handleClose}
-                        aria-label="close"
-                    >
-                        <CloseIcon/>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
             <Box sx={{
                 backgroundColor: '#000',
                 display: 'flex',
                 justifyContent: "center",
                 height: '100%',
-                color: '#fff'
+                color: '#fff',
+                mb: '45px',
+                p:1
             }}>
                 <SwipeableViews
                     index={activeStep}
@@ -81,42 +67,54 @@ const SlideShowSong = ({song, open, handleClose}: SlideShowSongPropsType) => {
                     {song.stanzas.map((step, index) => (
                         <div key={index}>
                             {Math.abs(activeStep - index) <= 2 ? (
-                                <Box>
+                                <>
                                     {step.lines.map((line, i) => (
-                                        <Typography key={i} sx={{whiteSpace: 'pre-wrap', p: 1, fontSize: "calc(36px + 3vh)"}}>
+                                        <Typography key={i}
+                                                    sx={{
+                                                        whiteSpace: 'pre-wrap',
+                                                        p: 1,
+                                                        fontSize: "calc(12px + 3vw)"
+                                                    }}>
                                             {line}
                                         </Typography>
                                     ))}
-                                </Box>
+                                </>
                             ) : null}
                         </div>
                     ))}
                 </SwipeableViews>
             </Box>
             <AppBar position="fixed" color="primary" sx={{top: 'auto', bottom: 0}}>
-                <Toolbar sx={{display: 'flex', justifyContent: 'center'}}>
-                    <MobileStepper
-                        steps={maxSteps}
-                        position="static"
-                        activeStep={activeStep}
-                        sx={{minWidth: 400}}
-                        nextButton={
-                            <Button
-                                size="small"
-                                onClick={handleNext}
-                                disabled={activeStep === maxSteps - 1}
-                            >
-                                Next
-                                <KeyboardArrowRight/>
-                            </Button>
-                        }
-                        backButton={
-                            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                                <KeyboardArrowLeft/>
-                                Back
-                            </Button>
-                        }
-                    />
+                <Toolbar sx={{display: 'flex', justifyContent: "space-between"}} variant="dense">
+                    <Typography sx={{ml: 2}} variant="h6" component="div">
+                        {song.title}
+                    </Typography>
+                    <div>
+                        <Button size="small"
+                                sx={{color: '#fff', mr: 2}}
+                                onClick={handleBack}
+                                disabled={activeStep === 0}>
+                            <KeyboardArrowLeft/>
+                            Back
+                        </Button>
+                        <Button
+                            size="small"
+                            sx={{color: '#fff', ml: 2}}
+                            onClick={handleNext}
+                            disabled={activeStep === maxSteps - 1}
+                        >
+                            Next
+                            <KeyboardArrowRight/>
+                        </Button>
+                    </div>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        onClick={handleClose}
+                        aria-label="close"
+                    >
+                        <CloseIcon/>
+                    </IconButton>
                 </Toolbar>
             </AppBar>
         </Dialog>
